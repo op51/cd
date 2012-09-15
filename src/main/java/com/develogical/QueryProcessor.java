b8a4676eb41c9ecd76336f86657613216bac2b7f
ovidiu <ovidiu@pitic.biz>
2012-09-15 15:20:00 +0200

    print input

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,10 @@ public class QueryProcessor {
 
     public String process(String query) {
     	
+    	System.out.println("###| INCOMING |###");
+    	System.out.println(query);
+    	System.out.println("###| -------- |###");
+    	
         if (query.contains("SPA2012")) {
             return "SPA is a conference";
         }
