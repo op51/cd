cc2e772b94d7f55e03dae9966f6f735594a8c767
ovidiu <ovidiu@pitic.biz>
2012-09-15 14:25:02 +0200

    sum ok

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,11 +3,20 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+    	
         if (query.contains("SPA2012")) {
             return "SPA is a conference";
         }
         if (query.contains("Dragos") || query.contains("Ovidiu")) {
         	return "I'm just a drop flop:" + query;
+        }
+        if (query.contains("what is")) {
+        	final String REGEXP= "([^\\d]+)([\\d]+)([^\\d]+)([\\d]+)";
+
+        	String[] r = query.split(REGEXP);
+
+        	double sum = new Double(r[1]).doubleValue() + new Double(r[3]).doubleValue();
+            return new Double(sum).toString();
         }
         return "";
     }
